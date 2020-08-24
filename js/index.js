225bdb5b3f3d3cf2eb6ac15bdd5f3a06b823a145
Katy Sage <katysagehj@gmail.com>
2020-08-24 16:05:45 -0400

    added get (fetch) JS file and working buttons

--- js/index.js
+++ js/index.js
@@ -1,2 +1,28 @@
+'use strict';
 const myButton = document.getElementById('refreshQuote');
-const para = document.getElementById('chuckSays');
+const chuckSays = document.getElementById('chuckSays');
+const submitForm = document.getElementById('submitForm');
+const defaultCategory = "dev"
+
+//function getQuote(category){}
+const getQuote = (category) => {
+    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+
+    get(url).then(function (fetchResponse){
+        chuckSays.innerHTML = fetchResponse.value;
+})
+    }
+myButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        getQuote(defaultCategory);
+})
+
+submitForm.addEventListener('click', function (e) {
+    e.preventDefault();
+    const userInput = document.getElementById("categoryInput");
+    const category= userInput.nodeValue;
+
+});
+(function () {
+    getQuote(defaultCategory)
+})();
